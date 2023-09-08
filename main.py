e5f93b0935ed8fd9be8dd4dee04d39948f6e9fcb
brijesh2001kumar <62830265+brijesh2001kumar@users.noreply.github.com>
2023-09-08 19:56:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -28,107 +28,109 @@ database = pickle.load(file_to_read)
 #Loading the required haarcascade_frontalface_default.xml” for face detection
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-# Making the Function decorator to memoize(cache) function execution of loading the model
-@st.cache
-def load_model_cached():
-    model = load_model('nn4.small2.lrn.h5',custom_objects = {'triplet_loss' : triplet_loss,'tf':tf})
-    return(model)
-
-# loading saved pre-trained model 
-model = load_model('nn4.small2.lrn.h5',custom_objects = {'triplet_loss' : triplet_loss,'tf':tf})
 
 
-# streamlit-webrtc Callback class for all the required frame processing for object detection
-# Input -> frame : input frame captured by webrtc-streamer
-# Output -> frame : processed input frame for object detection
-class VideoProcessor:
+# # Making the Function decorator to memoize(cache) function execution of loading the model
+# @st.cache
+# def load_model_cached():
+#     model = load_model('nn4.small2.lrn.h5',custom_objects = {'triplet_loss' : triplet_loss,'tf':tf})
+#     return(model)
 
-    # defining class objects
-    def __init__(self) -> None:
-        self.queu = ['!!Setting up!!'] # using queue to store 500 predictions at any time
-        self.stri = '' # vaiable to store prediction message
-        self.j = 0  # counter to control rate at which frames are processed
+# # loading saved pre-trained model 
+# model = load_model('nn4.small2.lrn.h5',custom_objects = {'triplet_loss' : triplet_loss,'tf':tf})
 
-    def recv(self, frame):
+
+# # streamlit-webrtc Callback class for all the required frame processing for object detection
+# # Input -> frame : input frame captured by webrtc-streamer
+# # Output -> frame : processed input frame for object detection
+# class VideoProcessor:
+
+#     # defining class objects
+#     def __init__(self) -> None:
+#         self.queu = ['!!Setting up!!'] # using queue to store 500 predictions at any time
+#         self.stri = '' # vaiable to store prediction message
+#         self.j = 0  # counter to control rate at which frames are processed
+
+#     def recv(self, frame):
         
-        error_message = 'Face Found'
+#         error_message = 'Face Found'
 
-        # step 1 : converting frame ('VideoFrame' Object  of 'pyAV' package) to numpy array
-        frm = frame.to_ndarray(format='bgr24')
-        # step 2 : flipping screen horizontally 
-        frm = cv2.flip(frm,1)
-        # step 3 : creating a copy of frame to use cascade classifier
-        img = frm
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+#         # step 1 : converting frame ('VideoFrame' Object  of 'pyAV' package) to numpy array
+#         frm = frame.to_ndarray(format='bgr24')
+#         # step 2 : flipping screen horizontally 
+#         frm = cv2.flip(frm,1)
+#         # step 3 : creating a copy of frame to use cascade classifier
+#         img = frm
+#         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#         faces = face_cascade.detectMultiScale(gray, 1.3, 5)
         
-        # Predicting using input frame
-        if len(faces)!=0:
-            i=1   # counter to predict for only one face in frame
-            for (x,y,w,h) in faces: # looping through coordinates of faces prdicted
-                if i==0:
-                    break
-                else :
-                    min_dist = 100
-                    self.j = self.j + 1
-                    # create a square bounding box around predicted face
-                    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-                    # condition to process only 1 frame out of 10
-                    if self.j % 10 ==0 :  
-                        # accessing the face part of image
-                        roi_color = img[y:y+h, x:x+w]  
-                        # resizing according to input size of model
-                        img = cv2.resize(roi_color, (96,96), interpolation=cv2.INTER_LINEAR)
-                        # converting image to bgr format
-                        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  
-                        # changing the order of image from (width,height,channels) to (channels,height,width)
-                        img = np.around(np.transpose(img, (0,1,2))/255.0, decimals=12)
-                        # converting cv2 object to array
-                        x_train = np.array([img])
-                        # finding the encoding vector 
-                        encoding = model.predict_on_batch(x_train)
+#         # Predicting using input frame
+#         if len(faces)!=0:
+#             i=1   # counter to predict for only one face in frame
+#             for (x,y,w,h) in faces: # looping through coordinates of faces prdicted
+#                 if i==0:
+#                     break
+#                 else :
+#                     min_dist = 100
+#                     self.j = self.j + 1
+#                     # create a square bounding box around predicted face
+#                     cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+#                     # condition to process only 1 frame out of 10
+#                     if self.j % 10 ==0 :  
+#                         # accessing the face part of image
+#                         roi_color = img[y:y+h, x:x+w]  
+#                         # resizing according to input size of model
+#                         img = cv2.resize(roi_color, (96,96), interpolation=cv2.INTER_LINEAR)
+#                         # converting image to bgr format
+#                         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  
+#                         # changing the order of image from (width,height,channels) to (channels,height,width)
+#                         img = np.around(np.transpose(img, (0,1,2))/255.0, decimals=12)
+#                         # converting cv2 object to array
+#                         x_train = np.array([img])
+#                         # finding the encoding vector 
+#                         encoding = model.predict_on_batch(x_train)
                         
-                        # looping through the saved encoding vectors to find best match 
-                        for (name, db_enc) in database.items():
-                            dist = np.linalg.norm(encoding-db_enc)
-                            if dist < min_dist:
-                                min_dist = dist
-                                identity = name[0:name.find('__')]
+#                         # looping through the saved encoding vectors to find best match 
+#                         for (name, db_enc) in database.items():
+#                             dist = np.linalg.norm(encoding-db_enc)
+#                             if dist < min_dist:
+#                                 min_dist = dist
+#                                 identity = name[0:name.find('__')]
                             
-                            # predicting unable to recognize is min_dist is less then 0.35
-                            if min_dist >= 0.35:
-                                if min_dist >= 0.95:
-                                    self.stri = 'Sorry!! Unable to recognize :('
+#                             # predicting unable to recognize is min_dist is less then 0.35
+#                             if min_dist >= 0.35:
+#                                 if min_dist >= 0.95:
+#                                     self.stri = 'Sorry!! Unable to recognize :('
 
-                            else:
-                                # appending new prediction to queue depending on the size of the queue 
-                                if len(self.queu)<=500:
-                                    self.queu.append(identity)
-                                else :
-                                    self.queu.pop(0)
-                                    self.queu.append(identity)
+#                             else:
+#                                 # appending new prediction to queue depending on the size of the queue 
+#                                 if len(self.queu)<=500:
+#                                     self.queu.append(identity)
+#                                 else :
+#                                     self.queu.pop(0)
+#                                     self.queu.append(identity)
 
-                                # giving final prediction based on the max frequency of the last 500 predictions
-                                max,res = max_freq_name(self.queu)
-                                if max==1:
-                                    self.stri = self.queu[0]
-                                elif max!=1:
-                                    self.stri = res
+#                                 # giving final prediction based on the max frequency of the last 500 predictions
+#                                 max,res = max_freq_name(self.queu)
+#                                 if max==1:
+#                                     self.stri = self.queu[0]
+#                                 elif max!=1:
+#                                     self.stri = res
 
-        else :
-            # message if cascade classifier is unable to find a face
-            error_message = 'Finding Face!!!!'
+#         else :
+#             # message if cascade classifier is unable to find a face
+#             error_message = 'Finding Face!!!!'
         
-        # Final display messages and their design
-        (label_width, label_height), baseline = cv2.getTextSize(self.stri, font, .5, 1)
-        (label_width_1, label_height_1), baseline = cv2.getTextSize(error_message, font, 1, 2)
-        cv2.rectangle(frm, (0,0),(frm.shape[1],40),(255,255,255),-1)
-        if error_message != 'Finding Face!!!!' : 
-            cv2.putText(frm, self.stri, (int(frm.shape[1]/2-label_width/2),20), font, 0.5, (0, 0, 0),1)
-        cv2.rectangle(frm,(0,frm.shape[0]-40),(frm.shape[1],frm.shape[0]),(255,255,255),-1)
-        cv2.putText(frm, error_message, (int(frm.shape[1]/2-label_width_1/2),frm.shape[0]-1), font, 1, (0, 0, 0),2)
+#         # Final display messages and their design
+#         (label_width, label_height), baseline = cv2.getTextSize(self.stri, font, .5, 1)
+#         (label_width_1, label_height_1), baseline = cv2.getTextSize(error_message, font, 1, 2)
+#         cv2.rectangle(frm, (0,0),(frm.shape[1],40),(255,255,255),-1)
+#         if error_message != 'Finding Face!!!!' : 
+#             cv2.putText(frm, self.stri, (int(frm.shape[1]/2-label_width/2),20), font, 0.5, (0, 0, 0),1)
+#         cv2.rectangle(frm,(0,frm.shape[0]-40),(frm.shape[1],frm.shape[0]),(255,255,255),-1)
+#         cv2.putText(frm, error_message, (int(frm.shape[1]/2-label_width_1/2),frm.shape[0]-1), font, 1, (0, 0, 0),2)
 
-        return av.VideoFrame.from_ndarray(frm, format='bgr24')
+#         return av.VideoFrame.from_ndarray(frm, format='bgr24')
 
 
 #---------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -170,24 +172,24 @@ with header:
     st.write("Face recognition is a 1:K way of identifying or confirming an individual's identity using their face . Facial Recognition systems can be used to identify photos, videos, or in real time. It has applications in mobile security system, Airport Immigration services, Attendence system, and many more ways.")
     st.markdown("""---""")
 
-with app:
+# with app:
 
-    st.subheader('The App')
+#     st.subheader('The App')
 
-    # streamlit-webrtc requires callbacks to process image and audio frames which is one major
-    # difference between OpenCV GUI and streamlit-webrtc
-    ctx = webrtc_streamer(key='key',
-                        # class object passed to video_processor_factory for video processing
-                        video_processor_factory=VideoProcessor,
-                        # rtc_configuration parameter to deploy the app to the cloud
-                        rtc_configuration=RTCConfiguration(
-                            {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}),
-                        # muting the audio input
-                        video_html_attrs=VideoHTMLAttributes(
-                            autoPlay=True, controls=True, style={"width": "100%"}, muted=True),
-                        )
+#     # streamlit-webrtc requires callbacks to process image and audio frames which is one major
+#     # difference between OpenCV GUI and streamlit-webrtc
+#     ctx = webrtc_streamer(key='key',
+#                         # class object passed to video_processor_factory for video processing
+#                         video_processor_factory=VideoProcessor,
+#                         # rtc_configuration parameter to deploy the app to the cloud
+#                         rtc_configuration=RTCConfiguration(
+#                             {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}),
+#                         # muting the audio input
+#                         video_html_attrs=VideoHTMLAttributes(
+#                             autoPlay=True, controls=True, style={"width": "100%"}, muted=True),
+#                         )
 
-    st.markdown("""---""")
+#     st.markdown("""---""")
 
 with video:
 
